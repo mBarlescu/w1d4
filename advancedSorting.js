665abe05bef1197b2d03247e7927f9aec4c5895c
Mark Barlescu <barlescu92@gmail.com>
2018-10-25 21:47:40 +0000

    sorting an array of objects, was super difficult, still dont even really understand it, just figured this out pretty much from examples online. dont understand the logic behind it at all

---
+++ advancedSorting.js
@@ -0,0 +1,26 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+var sortx = students.sort(function(a, b){
+  var nameA = a.name;
+  var nameB = b.name;
+  var ageA = a.age;
+  var ageB = b.age;
+
+  if(nameA < nameB){
+    return -1;
+  }
+  if(nameA > nameB){
+    return 1;
+  }
+  if(nameA === nameB){
+    return ageB - ageA;
+  }
+
+});
+
+console.log(sortx)
